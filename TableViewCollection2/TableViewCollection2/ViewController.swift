fe981d8bf535a4258571d015739a064ba9bb5c5c
VanDong <dongphuongbach2011@gmail.com>
2019-06-05 08:33:00 +0700

    init

---
+++ TableViewCollection2/TableViewCollection2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewCollection2
+//
+//  Created by Van Dong on 05/06/2019.
+//  Copyright © 2019 VanDong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
